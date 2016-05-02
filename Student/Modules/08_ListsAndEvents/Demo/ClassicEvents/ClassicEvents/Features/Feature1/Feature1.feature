9eedf3a2fc71616b480f7d1601e1ca691b0fbdd5
Ted Pattison <ted@tedpattison.net>
2016-05-01 23:52:39 -0400

    Updates

---
+++ Student/Modules/08_ListsAndEvents/Demo/ClassicEvents/ClassicEvents/Features/Feature1/Feature1.feature
@@ -0,0 +1,7 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="465242a4-018c-485c-9a9c-c0c5bcd4551b" featureId="465242a4-018c-485c-9a9c-c0c5bcd4551b" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="ClassicEvents Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="2d857c1a-42bd-4e4a-8aba-dc7e70cd5e8f" />
+    <projectItemReference itemId="11d31c0e-749e-4cd9-8b3f-f409f319d218" />
+  </projectItems>
+</feature>
---
+++ Student/Modules/08_ListsAndEvents/Demo/ClassicEvents/ClassicEvents/MoviesEventReceiver/MoviesEventReceiver.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace ClassicEvents.MoviesEventReceiver {
+
+    public class MoviesEventReceiver : SPItemEventReceiver {
+
+        public override void ItemAdding(SPItemEventProperties properties) {
+            string title = properties.AfterProperties["Title"].ToString();
+            if (title.Contains("&")) {
+                properties.Status = SPEventReceiverStatus.CancelWithError;
+                properties.ErrorMessage = "Don' use ampersand (&) in movie title";
+            }
+        }
+
+        public override void ItemUpdating(SPItemEventProperties properties) {
+            string title = properties.AfterProperties["Title"].ToString();
+            if (title.Contains("&")) {
+                properties.Status = SPEventReceiverStatus.CancelWithError;
+                properties.ErrorMessage = "Don' use ampersand (&) in movie title";
+            }
+        }
+
+        public override void ItemAdded(SPItemEventProperties properties) {
+            string title = properties.ListItem["Title"].ToString();
+            if (!title.ToUpper().Equals(title)) {
+                EventFiringEnabled = false;
+                properties.ListItem["Title"] = title.ToUpper();
+                properties.ListItem.UpdateOverwriteVersion();
+                EventFiringEnabled = true;
+            }
+        }
+
+        public override void ItemUpdated(SPItemEventProperties properties) {
+            string title = properties.ListItem["Title"].ToString();
+            if (!title.ToUpper().Equals(title)) {
+                EventFiringEnabled = false;
+                properties.ListItem["Title"] = title.ToUpper();
+                properties.ListItem.UpdateOverwriteVersion();
+                EventFiringEnabled = true;
+            }
+        }
+    }
+}
